e18ea70d02aa82c2ab8557bf6f3a1b51801d4bd7
Apurv <a0mish06@louisville.edu>
2025-09-24 22:49:07 -0400

    created vocabulary and character mappings

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ df = pd.read_csv('train.csv')
 text = " ".join(df['text'].dropna().astype(str)).lower()
 
 print(f"Total characters in text: `{len(text)}`")
+vocab = sorted(set(text))
+print(f"Vocabulary size: `{len(vocab)}`")
+
+char2idx = {c: i for i, c in enumerate(vocab)}
+idx2char = np.array(vocab)
+
+text_as_int = np.array([char2idx[c] for c in text])
