4cd8e4d435e26baf0ef9edc3e51ed87b98917c48
Apurv <a0mish06@louisville.edu>
2025-09-26 00:03:00 -0400

    pre-processing data

--- main.py
+++ main.py
@@ -15,3 +15,17 @@ char2idx = {c: i for i, c in enumerate(vocab)}
 idx2char = np.array(vocab)
 
 text_as_int = np.array([char2idx[c] for c in text])
+
+seq_length = 100
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+sequences = char_dataset.batch(seq_length + 1, drop_remainder=True)
+
+def split_input_target(chunk):
+    return chunk[:-1], chunk[1:]
+dataset = sequences.map(split_input_target)
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
